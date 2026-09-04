7f652803061252d42bbf06b8544194507cf6aea0
Ian Rodrigues <ianmrdrigues@gmail.com>
2026-09-04 02:37:00 -0300

    feat: configure PostgreSQL backend

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('TASKFLOW_DB_NAME', 'taskflow'),
+        'USER': os.environ.get('TASKFLOW_DB_USER', 'taskflow_user'),
+        'PASSWORD': os.environ['TASKFLOW_DB_PASSWORD'],
+        'HOST': os.environ.get('TASKFLOW_DB_HOST', '127.0.0.1'),
+        'PORT': os.environ.get('TASKFLOW_DB_PORT', '5432'),
     }
 }
 
